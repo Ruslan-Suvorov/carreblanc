5b91626163fc44a8519ffc64badabb50a21d606a
Ruslan Suvorov <bloodsrv@gmail.com>
2021-08-09 00:17:04 +0300

    update

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ $(function() {
       items: 5,
       margin: 100,
       loop: true,
-      nav: true
+      nav: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        200:{
+          items:1
+        },
+        1220:{
+          items:5
+        }
+      }
     });
     $(".next_button").click(function(){
         owl.trigger("next.owl.carousel");
@@ -15,3 +26,9 @@ $(function() {
         owl.trigger("prev.owl.carousel");
       });
   });
+
+  const menuBtn = document.getElementById('open-menu-btn');
+  const menu = document.querySelector('.menu-mobile');
+  const closeMenu = document.getElementById('close-menu-btn');
+  menuBtn.addEventListener('click', () => menu.style.display = 'flex')
+  closeMenu.addEventListener('click', () => menu.style.display = 'none')
